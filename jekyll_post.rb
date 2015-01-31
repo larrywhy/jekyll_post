d0e4653f74e9360ae0b47577fd74dde190c3f4bc
larrywhy <ks661354@gmail.com>
2015-01-31 21:56:36 +0800

    write test

--- jekyll_post.rb
+++ jekyll_post.rb
@@ -1,10 +1,20 @@
 require 'date'
-     
+
+puts "Your first variable is: #{ARGV[0]}"
+puts "Your second variable is: #{ARGV[1]}"
+puts "Your third variable is: #{ARGV[2]}"
+
 layout, title= ["_post","hello_jekyll"]
-     
-     
+
 puts "---"
 puts "layout: #{layout}"
 puts "title: \"#{title}\""
 puts "date: \"\""
 puts "---"
+
+
+
+File.open("aaa","w"){|file|
+    file.puts("line1")
+    file.puts("line2")
+}
