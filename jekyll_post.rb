3f7d2af1f2e261fc6a46d4a0e481d2fe52af36bb
larrywhy <ks661354@gmail.com>
2015-02-01 02:22:16 +0800

    version 1"

--- jekyll_post.rb
+++ jekyll_post.rb
@@ -1,20 +1,20 @@
 require 'date'
 
-puts "Your first variable is: #{ARGV[0]}"
-puts "Your second variable is: #{ARGV[1]}"
-puts "Your third variable is: #{ARGV[2]}"
+abort("Error! Too much arguments! Usage: jklpost <filename> [options].") if ARGV.length > 3
+title = "post"
+layout = "default"
+date = Date.today.to_s
+timeStamp = Time.now.strftime "%H%M%S"
 
-layout, title= ["_post","hello_jekyll"]
-
-puts "---"
-puts "layout: #{layout}"
-puts "title: \"#{title}\""
-puts "date: \"\""
-puts "---"
+title = ARGV[0] if ARGV.length == 1
+layout = ARGV[1] if ARGV.length == 2
+filename = date.to_s + "-" + title.to_s + "-" + timeStamp.to_s + ".md"
 
 
-
-File.open("aaa","w"){|file|
-    file.puts("line1")
-    file.puts("line2")
+File.open(filename.to_s,"w"){|file|
+    file.puts("---")
+    file.puts("layout: #{layout}")
+    file.puts("title: \"#{title}\"")
+    file.puts("date: #{Time.now.strftime "%Y-%m-%d %H:%M:%S"}")
+    file.puts("---")
 }
